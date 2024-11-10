529f7a01a5da26ed80893171038f709c6f52eb29
Rakesh giri <girirakesh7611@gmail.com>
2024-11-10 22:44:40 +0545

    update homestate screen

--- lib/presentation/screens/general/home/home.dart
+++ lib/presentation/screens/general/home/home.dart
@@ -10,12 +10,73 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          
-        ],
-      )
-    );
+    return Scaffold(
+        body: SafeArea(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        child: Column(
+          children: [
+            Image.asset(AppAssets.assetsImagenetflix).cornerRadius(20.r),
+            20.h.heightBox,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                'Latest Posts'.text.size(16).make(),
+                'See All'.text.size(16).make(),
+              ],
+            ),
+            20.h.heightBox,
+            ListView.separated(
+                itemCount: 5,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                separatorBuilder: (context, index) => SizedBox(
+                      height: 10.h,
+                    ),
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      Image.asset(
+                        AppAssets.assetsImagenetflix,
+                        height: 160.h,
+                        width: 220.h,
+                        fit: BoxFit.cover,
+                      ).cornerRadius(20),
+                      10.w.widthBox,
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          'Netflix will charge money for sharing the password'
+                              .text
+                              .maxLines(2)
+                              .size(16)
+                              .fontWeight(FontWeight.bold)
+                              .make(),
+                          6.h.heightBox,
+                          Row(
+                            children: [
+                              const Icon(FeatherIcons.clock),
+                              4.w.widthBox,
+                              '6 Months ago'.text.make()
+                            ],
+                          ),
+                          6.h.heightBox,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              '59 Views'.text.make(),
+                              const Icon(FeatherIcons.bookmark),
+                            ],
+                          )
+                        ],
+                      ).expand(),
+                    ],
+                  );
+                })
+          ],
+        ),
+      ),
+    ));
   }
 }
